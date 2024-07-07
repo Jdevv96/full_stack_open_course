8c398c39e43d4690e307ec7bb41e2bfc670081ff
Omar Tejada <omar14tejada@gmail.com>
2024-07-07 19:32:54 -0400

    submitting exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,1 +1,41 @@
+import {useState} from 'react'
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [positive, setPositive] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [negative, setNegative] = useState(0)
+
+  const handlePositiveClick = () => {
+    setPositive(positive + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleNegativeClick = () => {
+    setNegative(negative + 1)
+  }
+
+  return (
+    <div>
+      <h1>Feedback Form:</h1>
+      <Button handleClick={handlePositiveClick} text={'positive'} />
+      <Button handleClick={handleNeutralClick} text={'neutral'} />
+      <Button handleClick={handleNegativeClick} text={'negative'} />
+
+      <h2>Statistics: </h2>
+      <p>Positive: {positive} </p>
+      <p>Neutral: {neutral}</p>
+      <p>Negative: {negative}</p>
+    </div>
+  )
+}
+
+export default App
