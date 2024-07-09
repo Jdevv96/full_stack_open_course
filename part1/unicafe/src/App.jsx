c8c1cbbb4140b5f20c12be7bc823f3c024d31947
Omar Tejada <omar14tejada@gmail.com>
2024-07-09 15:40:44 -0400

    submitting unicafe 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,18 +10,37 @@ const App = () => {
   const [positive, setPositive] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [negative, setNegative] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percentPositive, setPercentPositive] = useState(0)
 
   const handlePositiveClick = () => {
-    setPositive(positive + 1)
+    const p = positive + 1
+    setPositive(p)
+    const t = total + 1
+    setTotal(t)
+    setAverage( (p - negative) / t)
+    setPercentPositive((p / t) * 100)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    const n = neutral + 1
+    setNeutral(n)
+    const t = total + 1
+    setTotal(t)
+    setAverage( (positive - negative) / t)
+    setPercentPositive((positive / t) * 100)
   }
 
   const handleNegativeClick = () => {
-    setNegative(negative + 1)
+    const n = negative + 1
+    setNegative(n)
+    const t = total + 1
+    setTotal(total + 1)
+    setAverage( (positive - n) / t)
+    setPercentPositive((positive / t) * 100)
   }
+
 
   return (
     <div>
@@ -34,6 +53,9 @@ const App = () => {
       <p>Positive: {positive} </p>
       <p>Neutral: {neutral}</p>
       <p>Negative: {negative}</p>
+      <p>Total: {total}</p>
+      <p>Average Score: {average}</p>
+      <p>Positive Score %: {percentPositive.toFixed(2)}%</p>
     </div>
   )
 }
