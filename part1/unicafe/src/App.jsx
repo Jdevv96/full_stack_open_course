b65239f1b808941e56a8a594e1aec83941f35d5c
Omar Tejada <omar14tejada@gmail.com>
2024-07-15 12:55:24 -0400

    sec 1, ex 1.9 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,58 +1,59 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const Statistics = (props) => {
-  const total = props.positive + props.negative + props.neutral
-  const avg = total >= 1 ? (props.positive - props.negative) / total : 0
-  const percentPositive = total >= 1 ? ((props.positive / total) * 100) : 0
-  return (
-    <div>
-      <p>Total: {total}</p>
-      <p>Average Score: {avg}</p>
-      <p>Percent Positive: {percentPositive}%</p>
-    </div>
-    
-  )
-}
+  const total = props.positive + props.negative + props.neutral;
+  const avg = total >= 1 ? (props.positive - props.negative) / total : 0;
+  const percentPositive = total >= 1 ? (props.positive / total) * 100 : 0;
 
+  if (total === 0) {
+    return <div>The app is used by pressing the buttons.</div>;
+  } else {
+    return (
+      <div>
+        <p>Positive: {props.positive} </p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Negative: {props.negative}</p>
+        <p>Total: {total}</p>
+        <p>Average Score: {avg}</p>
+        <p>Percent Positive: {percentPositive}%</p>
+      </div>
+    );
+  }
+};
 
 const App = () => {
-  const [positive, setPositive] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [negative, setNegative] = useState(0)
+  const [positive, setPositive] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [negative, setNegative] = useState(0);
 
   const handlePositiveClick = () => {
-    setPositive(positive + 1)
-  }
+    setPositive(positive + 1);
+  };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
 
   const handleNegativeClick = () => {
-    setNegative(negative + 1)
-  }
-
+    setNegative(negative + 1);
+  };
 
   return (
     <div>
       <h1>Feedback Form:</h1>
-      <Button handleClick={handlePositiveClick} text={'positive'} />
-      <Button handleClick={handleNeutralClick} text={'neutral'} />
-      <Button handleClick={handleNegativeClick} text={'negative'} />
+      <Button handleClick={handlePositiveClick} text={"positive"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleNegativeClick} text={"negative"} />
 
       <h2>Statistics: </h2>
-      <p>Positive: {positive} </p>
-      <p>Neutral: {neutral}</p>
-      <p>Negative: {negative}</p>
-      <Statistics positive={positive} negative={negative} neutral={neutral}/>
-    </div>
-  )
-}
 
-export default App
+      <Statistics positive={positive} negative={negative} neutral={neutral} />
+    </div>
+  );
+};
+
+export default App;
