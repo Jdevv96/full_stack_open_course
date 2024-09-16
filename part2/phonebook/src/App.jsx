8cabb95b43d6033128c67bf55a4cc3e6b6ca765d
Omar Tejada <omar14tejada@gmail.com>
2024-09-16 18:04:55 -0400

    part2-b, ex 2.9 step 4 complete.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,6 +5,8 @@ const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [personToFind, setPersonToFind] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   // event
   const addPerson = (event) => {
@@ -23,20 +25,27 @@ const App = (props) => {
     setNewNumber('')
   }
 
-  
-
-  // event handler
+  // event handlers
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearchChange = (event => {
+    setPersonToFind(event.target.value)
+    setShowAll(false)
+  })
+
+  // filter search
+  const personsToShow = showAll 
+  ? persons 
+  : persons.filter( person => person.name.toLowerCase().includes(personToFind.toLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={personToFind} onChange={handleSearchChange}/></div>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -44,7 +53,7 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <Person key={person.name} person={person}/>
         ))}
       </ul>
