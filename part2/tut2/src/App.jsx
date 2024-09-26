652f8b3a281e3042f575222c1bfde19c18bc7687
Omar Tejada <omar14tejada@gmail.com>
2024-09-25 22:32:10 -0400

    step2-e: walkthrough: adding initial styles. created notification component for error msg.

--- part2/tut2/src/App.jsx
+++ part2/tut2/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from './services/notes'
+import Notification from "./components/Notification"
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect( () => {
     noteService.getAll().then(initialNotes => {
@@ -19,8 +21,13 @@ const App = () => {
 
     noteService.update(id, changedNote).then( returnedNote => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-    }).catch( error => {
-      alert(`the note ${note.content} was already deleted from the server`)
+    }).catch( () => {
+      setErrorMessage(
+        `Note '${note.content}' was already removed from the server.`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes( notes.filter( n => n.id !== id))
     })
   }
@@ -49,6 +56,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
