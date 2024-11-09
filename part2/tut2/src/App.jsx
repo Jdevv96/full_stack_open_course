1da676aeee367778858073b5a1d6c3d5197063e0
Omar Tejada <omar14tejada@gmail.com>
2024-11-08 22:50:02 -0500

    correcting -make importance- toggle.

--- part2/tut2/src/App.jsx
+++ part2/tut2/src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
-      //setNotes( notes.filter( n => n.id !== id))
+      setNotes( notes.filter( n => n.id !== id))
     })
   }
 
